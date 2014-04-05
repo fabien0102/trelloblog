136127d64aa0ffa092076590bed99c88113aea79
François Monniot <francoismonniot@gmail.com>
2014-04-06 00:15:53 +0200

    Avoid global variable

--- js/controllers.js
+++ js/controllers.js
@@ -2,26 +2,24 @@
 
 /* Controllers */
 
-var trelloBlogControllers = angular.module( "trelloBlogControllers", [] );
+angular.module("trelloBlogControllers", [])
+  .controller("PostListCtrl", ["$scope",
+    function ($scope) {
+      var success = function (data) {
+        console.log(data);
+        $scope.posts = data;
+        $scope.$digest();
+      };
 
-trelloBlogControllers.controller( "PostListCtrl", ["$scope",
-  function ( $scope ) {
-    var success = function ( data ) {
-      console.log( data );
-      $scope.posts = data;
-      $scope.$digest();
-    };
+      var error = function (err) {
+        console.log(err);
+      };
 
-    var error = function ( err ) {
-      console.log( err );
-    };
+      Trello.lists.get("533f122757218a7e2c84c2cb/cards", success, error);
+    }
+  ])
+  .controller("PostDetailCtrl", ["$scope",
+    function ($scope) {
 
-    Trello.lists.get( "533f122757218a7e2c84c2cb/cards", success, error );
-  }
-] );
-
-trelloBlogControllers.controller( "PostDetailCtrl", ["$scope",
-  function ( $scope ) {
-
-  }
-] );
+    }
+  ]);
