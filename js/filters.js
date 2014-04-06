26393acb1c3800a44e46bb140b9b9a4fabdd8701
François Monniot <francoismonniot@gmail.com>
2014-04-06 18:16:27 +0200

    Filter post by language

--- js/filters.js
+++ js/filters.js
@@ -25,6 +25,34 @@ angular.module("trelloBlogFilters", [])
       });
     };
   } )
+  .filter("currentLanguage", ["config", function(config){
+
+    var filter;
+
+    if(config.multilingual) {
+      filter = function (posts, locale){
+        var currentLanguage = locale.split("-")[0].toLowerCase();
+
+        return _.filter(posts, function (post) {
+          var isCurrentLanguage = true;
+
+          _.each(post.labels, function (label) {
+            if (label.name.toLowerCase() === currentLanguage) {
+              isCurrentLanguage = false;
+            }
+          });
+
+          return !isCurrentLanguage;
+        });
+      }
+    } else {
+      filter = function (posts) {
+        return posts; // By default we accept all posts
+      };
+    }
+
+    return filter;
+  }])
   .filter( "i18n", ["I18n", function ( I18n ) {
     return function ( key ) {
       return I18n.translate( key );
