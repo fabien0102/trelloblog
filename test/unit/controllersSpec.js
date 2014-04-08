bd227fdb05266b257baa64e65424b0bd8c5ca3c3
François Monniot <francoismonniot@gmail.com>
2014-04-08 18:55:44 +0200

    Controllers tested

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,18 +4,42 @@ describe( "Taab controllers", function () {
 
   beforeEach( module( "trelloBlogApp" ) );
 
+  var Trello = {load: function () {}, blog: function () {return model}},
+    model = null;
+
   describe( "PostListCtrl", function () {
-    var scope, ctrl,
-      Trello = {load: function () {}, blog: function () {return model}},
-      model;
+    var scope, ctrl;
 
     beforeEach( inject( function ( $rootScope, $controller ) {
       scope = $rootScope.$new();
+      spyOn( Trello, "load" );
+
       ctrl = $controller( "PostListCtrl", {$scope: scope, Trello: Trello} );
     } ) );
 
     it( "should defined the date format", function () {
       expect( scope.dateFormat ).toEqual( "fullDate" );
+    } );
+
+    it( "should assign the model to the scope", function () {
+      expect( scope.trello ).toEqual( model );
+    } );
+
+    it( "should load the data from the service", function () {
+      expect( Trello.load ).toHaveBeenCalled()
+    } );
+  } );
+
+  describe( "AsideCtrl", function () {
+    var scope, ctrl;
+
+    beforeEach( inject( function ( $rootScope, $controller ) {
+      scope = $rootScope.$new();
+      ctrl = $controller( "AsideCtrl", {$scope: scope, Trello: Trello} );
+    } ) );
+
+    it( "should assign the model to the scope", function () {
+      expect( scope.trello ).toEqual( model );
     } );
   } );
 } );
