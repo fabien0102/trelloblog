a1441e2c4bf792e3b06fcb922e4b6f2fe760946e
François Monniot <francoismonniot@gmail.com>
2014-04-08 18:46:13 +0200

    Remove unused controller

--- js/controllers.js
+++ js/controllers.js
@@ -2,18 +2,13 @@
 
 /* Controllers */
 
-angular.module("trelloBlogControllers", [])
+angular.module( "trelloBlogControllers", [] )
   .controller( "PostListCtrl", ["$scope", "Trello", function ( $scope, Trello ) {
     $scope.dateFormat = 'fullDate';
 
     Trello.load();
     $scope.trello = Trello.blog();
-    }
-  ])
-  .controller("PostDetailCtrl", ["$scope",
-    function ($scope) {
-
-    }
+  }
   ] )
   .controller( "AsideCtrl", ["$scope", "Trello", function ( $scope, Trello ) {
     $scope.trello = Trello.blog();
