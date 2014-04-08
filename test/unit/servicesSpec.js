6d29b1632ce97806c77903bc3df3d949f87f1ed3
François Monniot <francoismonniot@gmail.com>
2014-04-08 19:12:52 +0200

    Trello service tested

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -66,5 +66,46 @@ describe( "Taab services", function () {
       expect( blog.labels ).toEqual( response.labelNames );
       expect( blog.cards ).toEqual( response.cards );
     } );
+
+    it( "should sort the cards by due date", function () {
+      $httpBackend.expectGET( requestUrl ).respond( {
+        cards: [
+          {due: "2014-03-06T12:00:00.000Z"},
+          {due: "2014-04-06T15:00:00.000Z"},
+          {due: "2014-04-06T12:00:00.000Z"}
+        ]
+      } );
+
+      service.load();
+      $httpBackend.flush();
+
+      var blog = service.blog();
+      expect( blog.cards[0].due ).toEqual( "2014-04-06T15:00:00.000Z" );
+      expect( blog.cards[1].due ).toEqual( "2014-04-06T12:00:00.000Z" );
+      expect( blog.cards[2].due ).toEqual( "2014-03-06T12:00:00.000Z" );
+    } );
+
+    it( "should add member information in each cards", function () {
+      $httpBackend.expectGET( requestUrl ).respond( {
+        cards: [
+          {idMembers: [ "51dad2ce8cdcf73a320018c5" ], due: 1},
+          {idMembers: [ "51843f636ef14b8a690062dc" ], due: 2},
+          {idMembers: [ "51843f636ef14b8a690062dc", "51dad2ce8cdcf73a320018c5" ], due: 3}
+        ],
+        members: [
+          { id: "51dad2ce8cdcf73a320018c5", username: "fmonniot" },
+          { id: "51843f636ef14b8a690062dc", username: "nicolascarlo" }
+        ]
+      } );
+
+      service.load();
+      $httpBackend.flush();
+
+      var blog = service.blog();
+      expect( blog.cards[0].members[0].username ).toEqual( "nicolascarlo" );
+      expect( blog.cards[0].members[1].username ).toEqual( "fmonniot" );
+      expect( blog.cards[1].members[0].username ).toEqual( "nicolascarlo" );
+      expect( blog.cards[2].members[0].username ).toEqual( "fmonniot" );
+    } );
   } );
 } );
