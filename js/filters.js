c58c33833f528dd558d276dcf838b902657e8f4b
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2014-04-08 10:29:47 +0200

    Add a tag filter to retrieve the tags checklist

--- js/filters.js
+++ js/filters.js
@@ -2,62 +2,66 @@
 
 /* Filters */
 
-angular.module("trelloBlogFilters", [])
-  .filter("notPastDue", function () {
-    return function (posts) {
-      if (_.isArray(posts)) {
-        return _.filter(posts, function (post) {
+angular.module( "trelloBlogFilters", [] )
+
+  .filter( "notPastDue", function () {
+    return function ( posts ) {
+      if ( _.isArray( posts ) ) {
+        return _.filter( posts, function ( post ) {
           return !_.isNull( post.due ) && new Date( post.due ).getTime() < Date.now();
-        });
+        } );
       }
     };
-  })
-  .filter("published", function () {
-    return function (posts) {
-      return _.filter(posts, function (post) {
+  } )
+
+  .filter( "published", function () {
+    return function ( posts ) {
+      return _.filter( posts, function ( post ) {
         var draft = false;
-        _.each(post.labels, function (label) {
+        _.each( post.labels, function ( label ) {
           if ( label.name.toLowerCase() === "unpublished" ) {
             draft = true;
           }
-        });
+        } );
         return !draft;
-      });
+      } );
     };
   } )
-  .filter("currentLanguage", ["config", function(config){
 
+  .filter( "currentLanguage", ["config", function ( config ) {
     var filter;
 
-    if(config.multilingual) {
-      filter = function (posts, locale){
-        var currentLanguage = locale.split("-")[0].toLowerCase();
+    if ( config.multilingual ) {
+      filter = function ( posts, locale ) {
+        var currentLanguage = locale.split( "-" )[0].toLowerCase();
 
-        return _.filter(posts, function (post) {
+        return _.filter( posts, function ( post ) {
           var isCurrentLanguage = true;
 
-          _.each(post.labels, function (label) {
-            if (label.name.toLowerCase() === currentLanguage) {
+          _.each( post.labels, function ( label ) {
+            if ( label.name.toLowerCase() === currentLanguage ) {
               isCurrentLanguage = false;
             }
-          });
+          } );
 
           return !isCurrentLanguage;
-        });
+        } );
       }
     } else {
-      filter = function (posts) {
+      filter = function ( posts ) {
         return posts; // By default we accept all posts
       };
     }
 
     return filter;
-  }])
+  }] )
+
   .filter( "i18n", ["I18n", function ( I18n ) {
     return function ( key ) {
       return I18n.translate( key );
     };
   }] )
+
   .filter( "lang", [function () {
     return function ( code ) {
       switch ( code ) {
@@ -68,5 +72,12 @@ angular.module("trelloBlogFilters", [])
       case "es":
         return "Español";
       }
+    };
+  }] )
+
+  .filter( "tags", [function () {
+    return function ( checklists ) {
+      var tagChecklist = _.filter( checklists, { name: "Tags" } )[0] || {checkItems: []};
+      return tagChecklist.checkItems;
     };
   }] );
