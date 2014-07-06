8a7b2ac03508357cad0b2683d1cbc0306a6dafba
Fabien0102 <fabien.bernard@telecom-em.eu>
2014-07-06 14:58:59 +0200

    Add configuration list

--- utils/config-cli.js
+++ utils/config-cli.js
@@ -9,6 +9,7 @@ if ( process.env.ENVIRONMENT === "test" ) {
   file = file.replace( "$BOARD$", "c94SaRKm" );
   file = file.replace( "$API_KEY$", "5af287a3734f0af280d09c2d3d0e3914" );
   file = file.replace( "$UNPUBLISHED_LABEL$", "unpublished" );
+  file = file.replace( "$CONFIGURATION_LIST$", "[Configuration]" );
   Fs.writeFileSync( "js/config.js", file );
   process.exit( 0 );
 }
@@ -33,14 +34,21 @@ Prompt.get( [
     name: "unpublishedLabel",
     description: "Name of unpublished label",
     default: "unpublished"
+  },
+  {
+    name: "configurationList",
+    description: "Name of configuration list",
+    default: "[Configuration]"
   }
 ], function ( err, prompt ) {
   var file = Fs.readFileSync( "js/config.js.dist" ).toString();
 
   file = file.replace( "$BOARD$", prompt.trelloBoardId );
   file = file.replace( "$API_KEY$", prompt.trelloApiKey );
-  file = file.replace( "$UNPUBLISHED_LABEL$", prompt.unpublishedLabel );
+  file = file.replace( "$UNPUBLISHED_LABEL$", prompt.unpublishedLabeêl );
+  file = file.replace( "$CONFIGURATION_LIST$", prompt.configurationList );
 
   Fs.writeFileSync( "js/config.js", file );
   console.log( "Congratulations! You are ready to change the world with your new blog!" );
+  console.log( "Just run `npm run publish` to publish your blog on a gh-pages ;)");
 } );
