d08a287d07b7afca50843ad9974c91d2320adddc
Fabien0102 <fabien.bernard@telecom-em.eu>
2014-07-06 16:27:48 +0200

    Refactor mixin with a $watch

--- js/directives.js
+++ js/directives.js
@@ -7,15 +7,22 @@ angular.module( "TrelloBlogDirectives", ["ngSanitize"] )
     var converter = new Showdown.converter();
     return {
       restrict: "AE",
-      scope: {
-        source: "=markdown"
-      },
       link: function ( scope, element, attributes ) {
-        var input = scope.source || element.html();
-        var output = input.replace( /:([a-z0-1-+]+):/g, function ( match, text ) {
-          return "<i class=\"emoji--" + text + "\" title=\" " + text + "\">" + text + "</i>"
-        } );
-        element.html( $sanitize(converter.makeHtml( output )) );
+        if (attributes.markdown) {
+          scope.$watch(attributes.markdown, function( input ) {
+            input = input || "";
+            var output = input.replace( /:([a-z0-1-+]+):/g, function ( match, text ) {
+              return "<i class=\"emoji--" + text + "\" title=\" " + text + "\">" + text + "</i>"
+            } );
+            element.html( $sanitize(converter.makeHtml( output )) );
+          } );
+        } else {
+          var input = element.html() || "";
+          var output = input.replace( /:([a-z0-1-+]+):/g, function ( match, text ) {
+            return "<i class=\"emoji--" + text + "\" title=\" " + text + "\">" + text + "</i>"
+          } );
+          element.html( $sanitize(converter.makeHtml( output )) );
+        }
       }
     }
   }] );
